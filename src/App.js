e0d0f88ac722155ed832063280a36c5abbb2979f
Ayush Pradhan <ayupradhan1999@gmail.com>
2021-08-18 16:46:51 +0530

    GA page view, user id, outbound clicks, scrolls, video, file downloads

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import Main from './components/MainComponent';
-import ReactGa from 'react-ga';
+// import ReactGA from 'react-ga';
 
 function App() {
-  var location = useState(window.location.pathname)
-  useEffect(() => {
-    ReactGa.initialize('G-E0TVGLDV3W')
-
-    // console.log('Reacttttttttttttttttttttttt Analyticsssssssssssssssssssssssssss')
-    // console.log(window.location.pathname + window.location.search)
-    
-    ReactGa.pageview(window.location.pathname + window.location.search)
-  }, [])
+  // var location = useState(window.location.pathname)
+  // useEffect(() => {
+  //   ReactGA.initialize('UA-161186843-1')
+  //   ReactGA.pageview(window.location.pathname + window.location.search)
+  // }, [])
   return (
     <div>
       <Main/>
