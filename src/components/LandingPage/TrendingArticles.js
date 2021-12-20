6a1ad81a8ca7dd54d2a6b9987e32e52ccbd4bced
Ayush Pradhan <ayupradhan1999@gmail.com>
2021-12-20 17:42:54 +0530

    UI

--- src/components/LandingPage/TrendingArticles.js
+++ src/components/LandingPage/TrendingArticles.js
@@ -7,24 +7,6 @@ const TrendingArticles = () => {
         <>
             <div className="trending-articles">
                 <div className="col-md-4 my-3">
-                <Link to="/cures/yoga">
-                    <div className="card d-flex justify-content-between rounded">
-                        <div className="h5 text-dark col-md-6 text-center">Yoga</div>
-                        <div className="col-md-6 rounded" id="yoga">                           
-                        </div>
-                    </div>
-                    </Link>
-                </div>
-                <div className="col-md-4 my-3">
-                    <Link to="/cures/Chinese">
-                        <div className="card d-flex justify-content-between">
-                            <div className="h5 text-dark col-md-6 text-center">Chinese<br/> Medicine</div>
-                            <div className="col-md-6 rounded" id="chinese">
-                            </div>
-                        </div>
-                    </Link>
-                </div>
-                <div className="col-md-4 my-3">
                     <Link to="/cures/Ayurveda">
                     <div className="card d-flex justify-content-between">
                         <div className="h5 text-dark col-md-6 text-center">Ayurveda</div>
@@ -33,25 +15,7 @@ const TrendingArticles = () => {
                     </div>
                     </Link>
                 </div>
-                
-                <div className="col-md-4 my-3">
-                    <Link to="/cures/Homeopathy">
-                    <div className="card d-flex justify-content-between">
-                        <div className="h5 text-dark col-md-6 text-center">Homeopathy</div>
-                        <div className="col-md-6 rounded" id="homeopathy">
-                        </div>
-                    </div>
-                    </Link>
-                </div>
-                <div className="col-md-4 my-3">
-                    <Link to="/cures/Naturopathy">
-                    <div className="card d-flex justify-content-between">
-                        <div className="h5 text-dark col-md-6 text-center">Naturopathy</div>
-                        <div className="col-md-6 rounded" id="naturopathy">
-                        </div>
-                    </div>
-                    </Link>
-                </div>
+
                 <div className="col-md-4 my-3">
                     <Link to="/cures/Unani">
                     <div className="card d-flex justify-content-between">
@@ -61,6 +25,47 @@ const TrendingArticles = () => {
                     </div>
                     </Link>
                 </div>
+
+                <div className="col-md-4 my-3">
+                    <Link to="/cures/Persian">
+                    <div className="card d-flex justify-content-between">
+                        <div className="h5 text-dark col-md-6 text-center">Persian</div>
+                        <div className="col-md-6 rounded" id="Persian">
+                        </div>
+                    </div>
+                    </Link>
+                </div>
+
+                <div className="col-md-4 my-3">
+                    <Link to="/cures/Chinese">
+                        <div className="card d-flex justify-content-between">
+                            <div className="h5 text-dark col-md-6 text-center">Chinese<br/> Medicine</div>
+                            <div className="col-md-6 rounded" id="chinese">
+                            </div>
+                        </div>
+                    </Link>
+                </div>
+
+                <div className="col-md-4 my-3">
+                    <Link to="/cures/Homeopathy">
+                    <div className="card d-flex justify-content-between">
+                        <div className="h5 text-dark col-md-6 text-center">Homeopathy</div>
+                        <div className="col-md-6 rounded" id="homeopathy">
+                        </div>
+                    </div>
+                    </Link>
+                </div>
+
+                <div className="col-md-4 my-3">
+                <Link to="/cures/yoga">
+                    <div className="card d-flex justify-content-between rounded">
+                        <div className="h5 text-dark col-md-6 text-center">Yoga</div>
+                        <div className="col-md-6 rounded" id="yoga">                           
+                        </div>
+                    </div>
+                    </Link>
+                </div>
+                
             </div>
         </>
     )
