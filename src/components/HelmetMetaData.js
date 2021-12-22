223c0f576f111a4f4c100c4342e99ab4659ef351
Ayush Pradhan <ayupradhan1999@gmail.com>
2021-12-22 15:11:53 +0530

    Og: description

--- src/components/HelmetMetaData.js
+++ src/components/HelmetMetaData.js
@@ -8,7 +8,7 @@ export default function HelmetMetaData(props) {
    let quote = props.quote !== undefined ? props.quote : "";
    let title = props.title !== undefined ? "All-Cures - "+props.title : "All-Cures - All in one Health App";
    let image = props.image !== undefined ? props.image : "https://all-cures.com/cures_articleimages/20211121_134401.jpg";
-   let description = props.description !== undefined ? props.description  : "We are a new age healthcare technology firm who are trying make it simple and convenient for the users to get information on Cures from anywhere in the world. With over 20 years of experience building technology products in the healthcare segment, our team is developing an integrated platform for all your health needs. We are passionate about giving our users the unique experience that is both fulfilling and wholesome.";
+   let description = props.description !== undefined ? props.description  : "All-Cures provides a social, centralized, user powered platform for alternate cures from across the globe. All-Cures is an alternative medicine information hub with the mission to democratize information about cures from diverse geographies. It covers all alternate systems of medicine including Ayurveda, Unani, Chinese and Persian.";
    let hashtag = props.hashtag !== undefined ? props.hashtag : "#allcures";
    console.log(props)
    return ( <Helmet>
