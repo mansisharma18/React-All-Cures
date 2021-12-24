7ffc7cd7de4652e16268a74c51b7e27c4d688486
hamza <hamalhamza@gmail.com>
2021-12-24 18:13:42 +0530

    carousel

--- src/components/LandingPage/Caousel1.js
+++ src/components/LandingPage/Caousel1.js
@@ -2,13 +2,14 @@ import React from 'react';
 import OwlCarousel from "react-owl-carousel";
 import "owl.carousel/dist/assets/owl.carousel.css";
 import "owl.carousel/dist/assets/owl.theme.default.css";
-import Slider1 from "../../assets/img/slider-1.png";
+import Arthritis from "../../assets/img/arthritis.png";
 import Slider2 from "../../assets/img/slider-2.png";
-import Slider3 from "../../assets/img/slider-3.png";
+import BloodPressure from "../../assets/img/bloodpressure.png";
 import Slider4 from "../../assets/img/slider-4.png";
+import Thyroid from "../../assets/img/thyroid.png";
+import Psoriasis from "../../assets/img/psoriasis.png";
+import Slider7 from "../../assets/img/insomnia.png";
 import Slider5 from "../../assets/img/slider-5.png";
-import Slider6 from "../../assets/img/slider-6.png";
-import Slider7 from "../../assets/img/slider-7.png";
 import { Link } from 'react-router-dom';
 
 const options = {
@@ -43,12 +44,12 @@ export default function Carousel1(props){
             <div className="tab-content category " id="choose-category" style={{maxWidth:"1140px", zIndex: 0}}>
                   <OwlCarousel className="owl-theme owl-loading" items={6} loop margin={150}  {...options}>
                   <div className="item bg-pink">
-                        <Link to={ `/searchcures/Cough And Cold` }>
+                        <Link to={ `/searchcures/Arthritis` }>
                            <div className="item-img">
-                              <img src={Slider1} alt="slider-img"/>
+                              <img src={Arthritis} alt="slider-img"/>
                            </div>
                            <div className="slider-heading">
-                              <p><strong>Cough</strong><br/> Cold</p>
+                              <p><strong>Arthritis</strong></p>
                            </div>
                         </Link>
                         </div>
@@ -58,24 +59,24 @@ export default function Carousel1(props){
                               <img src={Slider2} alt="slider-img"/>
                            </div>
                            <div className="slider-heading">
-                              <p><strong>Diabetes</strong><br/> Care</p>
+                              <p><strong>Diabetes</strong></p>
                            </div>
                            </Link>
                         </div>
                         <div className="item bg-l-org">
-                        <Link to={ `/searchcures/Cardiology` }>
+                        <Link to={ `/searchcures/Hypertension` }>
                            <div className="item-img">
-                              <img src={Slider3} alt="slider-img"/>
+                              <img src={BloodPressure} alt="slider-img"/>
                            </div>
                            <div className="slider-heading">
-                              <p><strong>Heart</strong> <br/>Care</p>
+                              <p><strong>Hypertension</strong></p>
                            </div>
                            </Link>
                         </div>
                         <div className="item bg-voilet">
                         <Link to={ `/searchcures/Insomnia` }>
                            <div className="item-img">
-                              <img src={Slider7} alt="slider-img" className='pb-4'/>
+                              <img src={Slider7} alt="slider-img" />
                            </div>
                            <div className="slider-heading">
                               <p><strong>Insomnia</strong><br/></p>
@@ -84,25 +85,35 @@ export default function Carousel1(props){
                         </div>
                         
                         <div className="item bg-blue">
-                        <Link to={ `/searchcures/Dermatology` }>
+                        <Link to={ `/searchcures/Thyroid` }>
                            <div className="item-img">
-                              <img src={Slider5} alt="slider-img"/>
+                              <img src={Thyroid} alt="slider-img"/>
                            </div>
                            <div className="slider-heading">
-                              <p><strong>Skin</strong> <br/>Care</p>
+                              <p><strong>Thyroid</strong></p>
                            </div>
                            </Link>
                         </div>
                         <div className="item bg-green">
-                        <Link to={ `/searchcures/ENT` }>
+                        <Link to={ `/searchcures/Psoriasis` }>
                            <div className="item-img">
-                              <img src={Slider6} alt="slider-img"/>
+                              <img src={Psoriasis} alt="slider-img"/>
                            </div>
                            <div className="slider-heading">
-                              <p><strong>Eye</strong> <br/>Care</p>
+                              <p><strong>Psoriasis</strong></p>
                            </div>
                            </Link>
                         </div> 
+                        <div className="item bg-l-org">
+                        <Link to={ `/searchcures/Skin Care` }>
+                           <div className="item-img">
+                              <img src={Slider5} alt="slider-img"/>
+                           </div>
+                           <div className="slider-heading">
+                              <p><strong>Skin</strong><br/>Care</p>
+                           </div>
+                        </Link>
+                        </div>
                         {/* <div className="navClass">></div>  */}
       </OwlCarousel>
       </div>
