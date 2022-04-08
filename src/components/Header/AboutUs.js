1461fc4e43c4271717096a8ffaa92e22a1e805dc
Nidhi Dhar <dnidhi.21@gmail.com>
2022-04-08 16:33:54 +0530

    worked on about us page

--- src/components/Header/AboutUs.js
+++ src/components/Header/AboutUs.js
@@ -30,15 +30,24 @@ export default class AboutUs extends Component{
 <div>
 <Header history={this.props.history}/>
 <div className="container my-4">
-    <h1>About Us</h1>
-    <p>All Cures is a product developed, managed and owned by Etherium Technologies. 
+    <h3>About Us</h3>
+    <p className='mt-3'>All Cures is a product developed, managed and owned by Etherium Technologies. 
         Our mission is to make it simple and convenient for users to get information on 
         Cures from anywhere in the world. Our belief is that your wellness is your well-being.
          We are passionate about giving our users the unique experience that is both fulfilling and wholesome.
          
          </p>
-         <div><a href="/feedback"><button id=""
-          className="article-search btn btn-dark mt-10">Submit Your Feedback</button></a></div><br/><br/>
+       
+
+          
+</div>
+<div className="container my-4">
+    <h3>Contact Us</h3>
+<h5 className='mt-3'>Email id: <a href="mailto:info@etheriumtech.com">info@etheriumtech.com</a></h5>
+<h5>Phone No.: <a href="tel:+91 191 295 9035">+91 191 295 9035</a></h5>
+<div><a href="/feedback"><button id=""
+          className="article-search btn btn-dark mt-10">Submit Your Feedback</button></a></div>
+          
 </div>
 <Footer></Footer>
 </div>
