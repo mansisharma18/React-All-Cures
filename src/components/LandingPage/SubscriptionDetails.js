16a834f3a71a09c423acf243d86c7da13f60b332
Nidhi Dhar <dnidhi.21@gmail.com>
2022-08-05 11:56:36 +0530

    worked on create order

--- src/components/LandingPage/SubscriptionDetails.js
+++ src/components/LandingPage/SubscriptionDetails.js
@@ -3,11 +3,14 @@ import Header from '../Header/Header';
 import axios from 'axios';
 import { backendHost } from '../../api-config';
 import Footer from '../Footer/Footer';
+
 import { faSortAmountDown } from '@fortawesome/free-solid-svg-icons';
 
 const SubscriptionDetails = (props) => {
   const [items, setItems] = useState([])
+  const [Loading, setLoading] = useState()
   const [amount, setAmount] = useState()
+  const [Id, setId] = useState()
  function subdetails(){
   axios.get(`${backendHost}/subscription/get`)
 
@@ -24,20 +27,69 @@ const SubscriptionDetails = (props) => {
  }
  useEffect(() => {
   subdetails()
+  
 }, [])
-
-
+function loadScript(src) {
+  return new Promise((resolve) => {
+    const script = document.createElement("script");
+    script.src = src;
+    script.onload = () => {
+      resolve(true);
+    };
+    script.onerror = () => {
+      resolve(false);
+    };
+    document.body.appendChild(script);
+  });
+}
 
  function displayRazorpay(priceId) {
+  
+  setLoading(true);
 axios.post(`${backendHost}/subscription/create_order`, {
    amount:priceId,
+  id:Id
+  })
+  .then(res => {
+   setId(JSON.parse(res.data).id); 
+   callOptions(); 
+   //setAmount(res.data)
+  })
+  
+  
+    .catch(err => {
+      console.log(err);
+    });
     
-  }).then(res=>{
-    console.log(res,res.data)
-  });
+function callOptions(){
 
- 
+var options = {
+  key: "rzp_test_GgDGBdRu7fT3hC",
+  currency: "INR",
+  amount: (priceId*100),//.toString(),
+  order_id:Id.toString,
+  name: "Subscription",
+  description: "Thank you for nothing. Please give us some money",
+  image: "http://localhost:1337/logo.svg",
+  handler: function (response) {
+    // alert(response.razorpay_payment_id);
+    // alert(response.razorpay_order_id);
+    // alert(response.razorpay_signature);
+
+    alert("Transaction successful");
+  },
+  prefill: {
+    name: "",
+    email: "",
+    phone_number: "",
+  },
+};
+const paymentObject = new window.Razorpay(options);
+    paymentObject.open();
  }
+}
+
+
   
   return (
    
