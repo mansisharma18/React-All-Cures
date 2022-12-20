6f07c4277f8889c98f19f029fa24286b582b0b19
Anil Raina <anilraina001@gmail.com>
2022-12-20 16:07:54 +0530

    restore old version for bar chart

--- src/components/Dashboard/BarAnalytics.js
+++ src/components/Dashboard/BarAnalytics.js
@@ -1,21 +1,23 @@
 import React from "react";
 import { ResponsiveContainer, XAxis, YAxis, CartesianGrid, Legend, Tooltip, BarChart, Bar} from "recharts";
 import { backendHost } from '../../api-config';
-class BarAnalytics extends React.Component {
-  
-	// Constructor
-	constructor(props) {
-		super(props);
 
-		this.state = {
-			barChartData: [],
-			DataisLoaded: false,
-            
-		};
-	}
+
+class BarAnalytics extends React.Component {
+
+        // Constructor
+        constructor(props) {
+                super(props);
+
+                this.state = {
+                        barChartData: [],
+                        DataisLoaded: false,
+
+                };
+        }
     componentDidMount() {
 
-        fetch(`${backendHost}/analytics/article`)
+        fetch(`${backendHost}/analytics/top`)
         .then((res)=> res.json())
         .then((json) => {
           this.setState({
@@ -23,31 +25,31 @@ class BarAnalytics extends React.Component {
           })
         })
         .catch(err => null
-        )  
+        )
      }
 
-   
-   
+
+
 
    render(){
     const { DataisLoaded, barChartData } = this.state;
-    
+
     return (
     <React.Fragment>
-      <h3 style={{color:"blue"}}>Course Bar chart</h3>
+      <h3 style={{color:"blue"}}>Top 10 Articles viewed since inception</h3>
       <ResponsiveContainer width="100%" aspect={2}>
        <BarChart data= {barChartData} margin={{left:50, right:50, top:100, bottom:100}}>
          <CartesianGrid strokeDasharray="2 2"/>
          <Tooltip />
-         <Bar dataKey="Count" fill="red" />
-         <Bar dataKey="article_id" fill="green" />
+         <Bar dataKey="No. of hits" fill="brown" />
+         {/* <Bar dataKey="article_id" fill="green" /> */}
          <XAxis dataKey ="article_id"  interval="preserveStartEnd" tickFormatter={(value)=> value}/>
          <YAxis />
          <Legend />
        </BarChart>
       </ResponsiveContainer>
     </React.Fragment>
-   
+
     );
 }
 }
