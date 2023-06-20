b2f0e40a0ad4877ad923912aae148d282af95890
arnavkoul-cyber <69846798+arnavkoul-cyber@users.noreply.github.com>
2023-06-20 10:31:15 +0530

    added condition you have already subscribed

--- src/components/Subscribe.js
+++ src/components/Subscribe.js
@@ -64,48 +64,50 @@ componentDidMount(){
         }, 5000);
       }
      
-     postSubscribtion() {
-   var phoneNumber = this.state.value.split('+')[1]
-   console.log(this.state.value)
-   var countryCodeLength = phoneNumber.length % 10
-   var countryCode = phoneNumber.slice(0, countryCodeLength)
-   var StringValue = phoneNumber.slice(countryCodeLength).replace(/,/g, '')
-   console.log(isValidPhoneNumber(this.state.value))
- 
-   if ( !isValidPhoneNumber(this.state.value)){
-      this.Alert('Please enter a 10-digit phone number!');
-      return; // Exit the function if the phone number is not 10 digits
-   }
-      this.setState({
-         afterSubmitLoad: true
-      })
-     axios.post(`${backendHost}/users/subscribe/${StringValue}`, {
-     "nl_subscription_disease_id": this.state.disease.join(','),
-     "nl_sub_type":1,
-     "nl_subscription_cures_id":this.state.cures.join(','),
-     "country_code": countryCode,
-     })
-       .then(res => {
-        this.setState({
-           afterSubmitLoad: false
-        })
-        if(res.data === 1){
-           this.Alert('You have successfully subscribed to our Newsletter')
+      postSubscribtion() {
+        var phoneNumber = this.state.value.split('+')[1];
+        console.log(this.state.value);
+        var countryCodeLength = phoneNumber.length % 10;
+        var countryCode = phoneNumber.slice(0, countryCodeLength);
+        var StringValue = phoneNumber.slice(countryCodeLength).replace(/,/g, '');
+        console.log(isValidPhoneNumber(this.state.value));
+      
+        if (!isValidPhoneNumber(this.state.value)) {
+          this.Alert('Please enter a 10-digit phone number!');
+          return; // Exit the function if the phone number is not 10 digits
         }
-        else {
-           this.Alert('Some error occured! Please try again later.')
-        }
-       })
-       .catch(err => {
-        this.setState({
-           afterSubmitLoad: false
-        })
-        this.Alert('Some error occured! Please try again later.')
         
-  
-     })
-     
- }
+        this.setState({
+          afterSubmitLoad: true
+        });
+        
+        axios
+          .post(`${backendHost}/users/subscribe/${StringValue}`, {
+            nl_subscription_disease_id: this.state.disease.join(','),
+            nl_sub_type: 1,
+            nl_subscription_cures_id: this.state.cures.join(','),
+            country_code: countryCode,
+          })
+          .then((res) => {
+            this.setState({
+              afterSubmitLoad: false
+            });
+      
+            if (res.data === 'Subscribed') {
+              this.Alert('You have successfully subscribed to our Newsletter');
+            } else if (res.data === 'Already subscribed') { // Add a check for 'already_subscribed' response
+              this.Alert('You are already subscribed to our Newsletter');
+            } else {
+              this.Alert('Some error occurred! Please try again later.');
+            }
+          })
+          .catch((err) => {
+            this.setState({
+              afterSubmitLoad: false
+            });
+            this.Alert('Some error occurred! Please try again later.');
+          });
+      }
       fetchCountriesCures = () => {
         fetch(`${backendHost}/isearch/treatmentregions/${this.state.items.disease_condition_id}`)
           .then((res)=> res.json())
